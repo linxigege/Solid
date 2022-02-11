b5a2b7143fa306c5914937cf01b8ecdc96252eab
o_liuxi <LIUXI520>
2022-02-11 16:10:05 +0800

    [modify] 使用的例子

--- src/main/java/com/github/zhangyingwei/solid/demo/Application.java
+++ src/main/java/com/github/zhangyingwei/solid/demo/Application.java
@@ -1,12 +1,23 @@
 package com.github.zhangyingwei.solid.demo;
 
+import com.github.zhangyingwei.solid.config.SolidConfiguration;
+import com.github.zhangyingwei.solid.config.StringTemplateResourceLoader;
+import com.github.zhangyingwei.solid.template.Template;
+import com.github.zhangyingwei.solid.template.TemplateResolver;
+
 /**
  * @author zhangyw
  * @date 2018/7/3
  */
 public class Application {
     public static void main(String[] args) {
-        String htmlTemplate = "Hello {{user.name}}";
-        System.out.println(htmlTemplate);
+        User user = new User();
+        user.setName("xiaoming");
+        user.setAge(29);
+        SolidConfiguration configuration = new SolidConfiguration(new StringTemplateResourceLoader());
+        TemplateResolver templateResolver = new TemplateResolver(configuration);
+        Template template = templateResolver.resolve("hello my name is {{user.name}} and my age is {{user.age}}.");
+        template.bind("user", user);
+        System.out.println(template.render());
     }
 }
